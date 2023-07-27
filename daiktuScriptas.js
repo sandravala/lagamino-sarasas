d7cbad0d5345dcfe3e0df129539d39c071518092
sandravala <115172535+sandravala@users.noreply.github.com>
2023-07-27 21:20:10 +0300

    Update daiktuScriptas.js

--- daiktuScriptas.js
+++ daiktuScriptas.js
@@ -1,5 +1,7 @@
 let rangeInputs = document.getElementsByClassName('formbox__field-range');
 let titles = document.getElementsByClassName('formbox__title');
+let resultBlock = document.getElementsByClassName('has-result')[0];
+resultBlock.setAttribute('id', 'is-hidden');
 
 rangeInputs[0].addEventListener("input", function(){
 	let value = rangeInputs[0].value > 14 ? 'daugiau, nei 14' : rangeInputs[0].value;
@@ -10,3 +12,8 @@ rangeInputs[1].addEventListener("input", function(){
 	let value = rangeInputs[1].value > 25 ? 'daugiau, nei 25' : rangeInputs[1].value;
 	titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ' + value + '°C';
 });
+
+let buttonGenerate = document.getElementsByClassName('formbox__btn-calc')[0];
+buttonGenerate.addEventListener("click", function(){
+document.getElementsByClassName('has-result')[0].removeAttribute('id');
+})
