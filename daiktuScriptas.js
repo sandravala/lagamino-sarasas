911a60bfd2370a882f6a75aa14543db84c5be963
sandravala <115172535+sandravala@users.noreply.github.com>
2023-07-28 00:56:02 +0300

    Update daiktuScriptas.js

--- daiktuScriptas.js
+++ daiktuScriptas.js
@@ -3,28 +3,6 @@ let titles = document.getElementsByClassName('formbox__title');
 let resultBlock = document.getElementsByClassName('has-result')[0];
 resultBlock.setAttribute('id', 'is-hidden');
 
-rangeInputs[0].addEventListener("input", function(){
-	let value = rangeInputs[0].value > 14 ? 'daugiau, nei 14' : rangeInputs[0].value;
-	titles[0].innerHTML = 'Atostogų trukmė: ' + value + ' d.';
-});
-
-rangeInputs[1].addEventListener("input", function(){
-	let value = rangeInputs[1].value > 25 ? 'daugiau, nei 25' : rangeInputs[1].value;
-	titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ' + value + '°C';
-});
-
-let buttonGenerate = document.getElementsByClassName('formbox__btn-calc')[0];
-buttonGenerate.addEventListener("click", function(){
-document.getElementsByClassName('has-result')[0].removeAttribute('id');
-})
-
-let buttonReset = document.getElementsByClassName('formbox__btn-reset')[0];
-buttonReset.addEventListener("click", function(){
-document.getElementsByClassName('has-result')[0].setAttribute('id', 'is-hidden');
-titles[0].innerHTML = 'Atostogų trukmė: ';
-titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ';
-})
-
 let printBtn = 
 `
 <div class="formbox__actions_btns">
@@ -41,4 +19,30 @@ let printBtn =
 </div><input type="hidden" id="print_calculator_13" name="print_calculator_13" value="32701e0098"><input type="hidden" name="_wp_http_referer" value="/lagamino-turinys/"></div>
 `;
 
-document.getElementsbyClassName('formbox-wrapper')[0].appendChild(printBtn);
+let forma = document.getElementsbyClassName('formbox-wrapper')[0];
+
+
+rangeInputs[0].addEventListener("input", function(){
+	let value = rangeInputs[0].value > 14 ? 'daugiau, nei 14' : rangeInputs[0].value;
+	titles[0].innerHTML = 'Atostogų trukmė: ' + value + ' d.';
+});
+
+rangeInputs[1].addEventListener("input", function(){
+	let value = rangeInputs[1].value > 25 ? 'daugiau, nei 25' : rangeInputs[1].value;
+	titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ' + value + '°C';
+});
+
+let buttonGenerate = document.getElementsByClassName('formbox__btn-calc')[0];
+buttonGenerate.addEventListener("click", function(){
+document.getElementsByClassName('has-result')[0].removeAttribute('id');
+	forma.appendChild(printBtn);
+})
+
+let buttonReset = document.getElementsByClassName('formbox__btn-reset')[0];
+buttonReset.addEventListener("click", function(){
+document.getElementsByClassName('has-result')[0].setAttribute('id', 'is-hidden');
+titles[0].innerHTML = 'Atostogų trukmė: ';
+titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ';
+})
+
+
