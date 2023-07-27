232cdd711920a45362daf00987fdd83015ee3b8d
sandravala <115172535+sandravala@users.noreply.github.com>
2023-07-28 01:21:29 +0300

    Update daiktuScriptas.js

--- daiktuScriptas.js
+++ daiktuScriptas.js
@@ -3,6 +3,20 @@ let titles = document.getElementsByClassName('formbox__title');
 let resultBlock = document.getElementsByClassName('has-result')[0];
 resultBlock.setAttribute('id', 'is-hidden');
 
+let printBtn = `
+<div class="formbox__actions_btns"><div class="formbox__actions">
+					<button class="formbox__action-btn formbox__btn-link" id="formbox-btn-link-13">
+					<span class="cb-icon-link"></span>
+					<span class="cb__tooltip">Kopijuoti nuorodą</span>
+					</button>
+					<div class="formbox__footer-separate">|</div>
+					<button class="formbox__action-btn formbox__btn-print" name="cb_view" value="13">
+					<span class="cb-icon-print"></span>
+					<span class="cb__tooltip">Spausdinti</span>
+					</button>
+				</div><input type="hidden" id="print_calculator_13" name="print_calculator_13" value="378024d43c"><input type="hidden" name="_wp_http_referer" value="/atostogos-ka-isideti-i-lagamina/"></div>
+`;
+
 rangeInputs[0].addEventListener("input", function(){
 	let value = rangeInputs[0].value > 14 ? 'daugiau, nei 14' : rangeInputs[0].value;
 	titles[0].innerHTML = 'Atostogų trukmė: ' + value + ' d.';
@@ -16,6 +30,7 @@ rangeInputs[1].addEventListener("input", function(){
 let buttonGenerate = document.getElementsByClassName('formbox__btn-calc')[0];
 buttonGenerate.addEventListener("click", function(){
 document.getElementsByClassName('has-result')[0].removeAttribute('id');
+document.getElementsByClassName('has-result')[0].appendChild(printBtn);	
 })
 
 let buttonReset = document.getElementsByClassName('formbox__btn-reset')[0];
