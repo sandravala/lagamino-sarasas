257d56d9114e3ad5d23d6c78d82916d1c1b17c9e
sandravala <115172535+sandravala@users.noreply.github.com>
2023-08-02 17:49:38 +0300

    Update daiktuScriptas.js

--- daiktuScriptas.js
+++ daiktuScriptas.js
@@ -67,7 +67,7 @@ function printData() {
 	            	</div>`;
 	
 	const dataToPrint = document.getElementById('for-printing');
-	const url = location.protocol + '//' + location.host + location.pathname + '?print-window=1';
+	const url = location.protocol + '//' + location.host + location.pathname;
 	newWin = window.open(url, "_blank");
 	newWin.document.write('<html lang="lt-LT">');
 	newWin.document.write(newHead);
