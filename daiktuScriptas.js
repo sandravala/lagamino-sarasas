cf116f943c6fb981392477ddeb9ccac3f1685ed6
sandravala <115172535+sandravala@users.noreply.github.com>
2023-08-02 10:19:31 +0300

    Update daiktuScriptas.js

--- daiktuScriptas.js
+++ daiktuScriptas.js
@@ -1,39 +1,6 @@
 let rangeInputs = document.getElementsByClassName('formbox__field-range');
 let titles = document.getElementsByClassName('formbox__title');
 let resultBlock = document.getElementsByClassName('has-result')[0];
-
-if (window.location.search.includes('print_calculator')) {
-	//cia pasidarau pavadinima
-	const newDiv = document.createElement('div');
-	newDiv.innerHTML = 'pavadinimas';
-	const result = document.getElementsByName('formbox')[0];
-	result.insertBefore(newDiv, result.firstChild);
-
-	//istrinam nereikalingus elementus
-	const formContainers = document.querySelectorAll('.formbox__container');
-	const filteredContainers = Array.from(formContainers).filter((element) => {
-		return element.classList.length === 1 && element.classList.contains('formbox__container');
-	});
-	if (filteredContainers.length > 0) {
-	    for (let i = 0; i < filteredContainers.length - 1; i++) {
-	    filteredContainers[i].parentNode.removeChild(filteredContainers[i]);
-	  }
-	}
-	
-	//nuimam hidden atributa nuo saraso
-	resultBlock.removeAttribute('id');
-
-	//pakeiciam mygtuku teksta ir failo pavadinima
-	document.querySelectorAll('.button.pdf')[0].innerHTML('Parsisiųsti');
-	document.querySelectorAll('.button.print')[0].innerHTML('Spausdinti');
-	document.querySelectorAll('.button.pdf')[0].setAttribute('data-name','12GM-Daiktu-sarasas-atostogoms.pdf');
-
-} else {
-	resultBlock.setAttribute('id', 'is-hidden');
-	// cia istrinu result title, kad liktu tik saraso blokas
-	document.querySelectorAll('.formbox__container.has-result')[0].removeChild(document.querySelectorAll('.formbox__container.has-result')[0].firstChild);
-}
-
 let touched = 0;
 
 let printBtn = `
@@ -50,38 +17,78 @@ let printBtn = `
 				</div><input type="hidden" id="print_calculator_13" name="print_calculator_13" value="378024d43c"><input type="hidden" name="_wp_http_referer" value="/atostogos-ka-isideti-i-lagamina/"></div>
 `;
 
+if (window.location.search.includes('print_calculator')) {
 
+	document.addEventListener('DOMContentLoaded', function() {
 
-rangeInputs[0].addEventListener("input", function(){
-	let value = rangeInputs[0].value > 14 ? 'daugiau, nei 14' : rangeInputs[0].value;
-	titles[0].innerHTML = 'Atostogų trukmė: ' + value + ' d.';
-	touched = 1;
-});
+		//cia pasidarau pavadinima
+		const newDiv = document.createElement('div');
+		newDiv.innerHTML = 'pavadinimas';
+		const result = document.getElementsByName('formbox')[0];
+		result.insertBefore(newDiv, result.firstChild);
+	
+		//istrinam nereikalingus elementus
+		const formContainers = document.querySelectorAll('.formbox__container');
+		const filteredContainers = Array.from(formContainers).filter((element) => {
+			return element.classList.length === 1 && element.classList.contains('formbox__container');
+		});
+		if (filteredContainers.length > 0) {
+		    for (let i = 0; i < filteredContainers.length - 1; i++) {
+		    filteredContainers[i].parentNode.removeChild(filteredContainers[i]);
+		  }
+		}
+		
+		//nuimam hidden atributa nuo saraso
+		resultBlock.removeAttribute('id');
+	
+		//pakeiciam mygtuku teksta ir failo pavadinima
+		document.querySelectorAll('.button.pdf')[0].innerHTML('Parsisiųsti');
+		document.querySelectorAll('.button.print')[0].innerHTML('Spausdinti');
+		document.querySelectorAll('.button.pdf')[0].setAttribute('data-name','12GM-Daiktu-sarasas-atostogoms.pdf');
+	});
 
-rangeInputs[1].addEventListener("input", function(){
-	let value = rangeInputs[1].value > 25 ? 'daugiau, nei 25' : rangeInputs[1].value;
-	titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ' + value + '°C';
-	touched = 1;
-});
-
-function pridetiPrintBtn() {
-document.getElementsByClassName('formbox__title')[4].innerHTML = printBtn;
+} else {
+	resultBlock.setAttribute('id', 'is-hidden');
+	// cia istrinu result title, kad liktu tik saraso blokas
+	document.querySelectorAll('.formbox__container.has-result')[0].removeChild(document.querySelectorAll('.formbox__container.has-result')[0].firstChild);
+	
+	rangeInputs[0].addEventListener("input", function(){
+		let value = rangeInputs[0].value > 14 ? 'daugiau, nei 14' : rangeInputs[0].value;
+		titles[0].innerHTML = 'Atostogų trukmė: ' + value + ' d.';
+		touched = 1;
+	});
+	
+	rangeInputs[1].addEventListener("input", function(){
+		let value = rangeInputs[1].value > 25 ? 'daugiau, nei 25' : rangeInputs[1].value;
+		titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ' + value + '°C';
+		touched = 1;
+	});
+	
+	function pridetiPrintBtn() {
+	document.getElementsByClassName('formbox__title')[4].innerHTML = printBtn;
+	}
+	
+	let buttonGenerate = document.getElementsByClassName('formbox__btn-calc')[0];
+	buttonGenerate.addEventListener("click", function(){
+	document.getElementsByClassName('has-result')[0].removeAttribute('id');
+	
+	//pridetiPrintBtn();	
+	})
+	
+	let buttonReset = document.getElementsByClassName('formbox__btn-reset')[0];
+	buttonReset.addEventListener("click", function(){
+	document.getElementsByClassName('has-result')[0].setAttribute('id', 'is-hidden');
+	titles[0].innerHTML = 'Atostogų trukmė: ';
+	titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ';
+	})
+	
+	function rangesTouched() {
+	return !!touched;
+	}
 }
 
-let buttonGenerate = document.getElementsByClassName('formbox__btn-calc')[0];
-buttonGenerate.addEventListener("click", function(){
-document.getElementsByClassName('has-result')[0].removeAttribute('id');
 
-//pridetiPrintBtn();	
-})
 
-let buttonReset = document.getElementsByClassName('formbox__btn-reset')[0];
-buttonReset.addEventListener("click", function(){
-document.getElementsByClassName('has-result')[0].setAttribute('id', 'is-hidden');
-titles[0].innerHTML = 'Atostogų trukmė: ';
-titles[1].innerHTML = 'Vidutinė temperatūra viešnagės metu: ';
-})
 
-function rangesTouched() {
-return !!touched;
-}
+
+
