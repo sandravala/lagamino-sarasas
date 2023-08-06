6d97b7f9967da09cd6132b1a3af97179ef4b451f
sandravala <115172535+sandravala@users.noreply.github.com>
2023-08-06 14:25:53 +0300

    Update daiktuScriptas.js

--- daiktuScriptas.js
+++ daiktuScriptas.js
@@ -62,7 +62,7 @@ const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="1
 </svg>`;
 
 const addListItemInput = `<div class="is-hidden">
- 			<input type="text">
+ 			<input type="text" class="dont-print">
 				<button type="button" class="listBtn plusButton dont-print" style="background-color: #ffffff; color: #1c7f18; border: none;">
     					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 12 12">
 					  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
