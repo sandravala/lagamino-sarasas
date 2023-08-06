b1bcac30ec2acd6762ef8a34465964d6ef3b50a6
sandravala <115172535+sandravala@users.noreply.github.com>
2023-08-06 13:40:30 +0300

    Update daiktuScriptas.js

--- daiktuScriptas.js
+++ daiktuScriptas.js
@@ -61,6 +61,17 @@ const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="1
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg>`;
 
+const addListItemInput = `<div class=">
+ 			<input type="text">
+				<button type="button" class="plusButton dont-print" style="background-color: #ffffff; color: #1c7f18; border: none;">
+    					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 12 12">
+					  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+					</svg>
+    				</button>
+			</div>
+   			<div><button type="button" id="addItem" class="plusButton dont-print" style="background-color: #ffffff; color: #1c7f18; border: none;">Pridėti</button></div>
+  `;
+
 function generuotiEilute(tekstas, i) {
 	
 	return `<div>
