070be4572acee87632a0f6f96287e7be82681618
sandravala <115172535+sandravala@users.noreply.github.com>
2023-08-06 14:11:33 +0300

    Update daiktuScriptas.js

--- daiktuScriptas.js
+++ daiktuScriptas.js
@@ -63,13 +63,13 @@ const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="1
 
 const addListItemInput = `<div class=">
  			<input type="text">
-				<button type="button" class="plusButton dont-print" style="background-color: #ffffff; color: #1c7f18; border: none;">
+				<button type="button" class="listBtn plusButton dont-print" style="background-color: #ffffff; color: #1c7f18; border: none;">
     					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 12 12">
 					  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
 					</svg>
     				</button>
 			</div>
-   			<div><button type="button" id="addItem" class="plusButton dont-print" style="background-color: #ffffff; color: #1c7f18; border: none;">Pridėti</button></div>
+   			<div><button type="button" class="listBtn addButton dont-print" style="background-color: #ffffff; color: #1c7f18; border: none;">Pridėti</button></div>
   `;
 
 function generuotiEilute(tekstas, i) {
@@ -77,7 +77,7 @@ function generuotiEilute(tekstas, i) {
 	return `<div>
  			<input type="checkbox" id="${getFirstWord(tekstas) + i}" class="myCheckbox">
     				<label id="rLabel" for="${getFirstWord(tekstas) + i}">${tekstas}</label>
-				<button type="button" class="minusButton dont-print" style="background-color: #ffffff; color: #a90909; border: none;">${deleteIcon}</button>
+				<button type="button" class="listBtn minusButton dont-print" style="background-color: #ffffff; color: #a90909; border: none;">${deleteIcon}</button>
 		</div>`;
 }
 
